d7c3cfe0fed3410d99685c5e4f420a188d68e4c7
Leonardo Jorge <leogojorge@outlook.com>
2025-04-02 17:06:33 -0300

    :construction: Starts to develop HashTable

---
+++ DataStructure/HashTable.cs
@@ -0,0 +1,139 @@
+﻿namespace DataStructure
+{
+    public class HashTable
+    {
+        public string[] Bucket;
+
+        public int Capacity { get; init; }
+
+        public int HashModulator { get; init; }
+
+        public HashTable(int capacity)
+        {
+            this.Capacity = capacity * 2;
+            this.Bucket = new string[this.Capacity];
+            this.HashModulator = this.Capacity - 1;
+        }
+
+        public int GetHashCode(string key)
+        {
+            int hashTotalNumber = 0;
+
+            foreach (var item in key.ToCharArray())
+            {
+                hashTotalNumber += item;
+            }
+
+            return hashTotalNumber % HashModulator;
+        }
+
+        public bool Add(string value)
+        {
+            int hashCode = this.GetHashCode(value);
+
+            var node = this.Bucket[hashCode];
+
+            if (node != null)
+            {
+                //adiciona no proximo espaço livre
+            }
+            else
+            {
+                this.Bucket[hashCode] = value;
+            }
+
+            return true;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < this.Bucket.Length; i++)
+            {
+                Console.WriteLine(i + " : " + this.Bucket[i]);
+            }
+        }
+    }
+
+    public class HashTableNode
+    {
+        public int Key;
+        public string Value;
+
+        public HashTableNode(int key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
+
+
+//namespace DataStructure
+//{
+//    public class HashTable
+//    {
+//        public HashTableNode[] Bucket;
+
+//        public int Capacity { get; init; }
+
+//        public int HashModulator { get; init; }
+
+
+//        public HashTable(int capacity)
+//        {
+//            this.Capacity = capacity * 2;
+//            this.Bucket = new HashTableNode[this.Capacity];
+//            this.HashModulator = this.Capacity - 1;
+//        }
+
+//        public int GetHashCode(string key)
+//        {
+//            int hashTotalNumber = 0;
+
+//            foreach (var item in key.ToCharArray())
+//            {
+//                hashTotalNumber += item;
+//            }
+
+//            return hashTotalNumber % HashModulator;
+//        }
+
+//        public bool Add(string value)
+//        {
+//            int hashCode = this.GetHashCode(value);
+
+//            var node = this.Bucket[hashCode];
+
+//            if (node != null)
+//            {
+//                //adiciona no proximo espaço livre
+//            }
+//            else
+//            {
+//                this.Bucket[hashCode] = new HashTableNode(hashCode, value);
+//            }
+
+//            return true;
+//        }
+
+//        public void Print()
+//        {
+//            for (int i = 0; i < this.Bucket.Length; i++)
+//            {
+//                Console.WriteLine(this.Bucket[i].Key + " : " + this.Bucket[i].Value);
+//            }
+//        }
+//    }
+
+//    public class HashTableNode
+//    {
+//        public int Key;
+//        public string Value;
+
+//        public HashTableNode(int key, string value)
+//        {
+//            this.Key = key;
+//            this.Value = value;
+//        }
+//    }
+//}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,11 +1,108 @@
-﻿namespace DataStructure
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DataStructure
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            OperateOnSinlgyLinkedList();
+            OperateOnHashTable();
+            //OperateOnDoublyLinkedList();
+            //OperateOnSinlgyLinkedList();
             //OperateOnStackArray();
+        }
+        private static void OperateOnHashTable()
+        {
+            int hashTableCapacity = 100;
+            var hashTable = new HashTable(hashTableCapacity);
+            hashTable.Add("ana");
+            hashTable.Add("leo");
+            hashTable.Add("fat");
+            hashTable.Add("cel");
+            hashTable.Add("ade");
+            hashTable.Add("car");
+            hashTable.Add("nic");
+            hashTable.Add("bre");
+
+            hashTable.Print();
+        }
+
+        private static void OperateOnDoublyLinkedList()
+        {
+            var doublyLinkedList = new DoublyLinkedList<int>();
+
+            doublyLinkedList.Add(3);
+            doublyLinkedList.Add(1);
+            doublyLinkedList.Add(4);
+            doublyLinkedList.Add(8);
+
+            doublyLinkedList.Print();
+
+            Console.WriteLine(doublyLinkedList.Find(3));
+            Console.WriteLine(doublyLinkedList.Find(1));
+            Console.WriteLine(doublyLinkedList.Find(4));
+            Console.WriteLine(doublyLinkedList.Find(8));
+            Console.WriteLine(doublyLinkedList.Find(0));
+
+            Console.WriteLine(doublyLinkedList.Remove(3));
+            Console.WriteLine(doublyLinkedList.Remove(1));
+            Console.WriteLine(doublyLinkedList.Remove(4));
+            Console.WriteLine(doublyLinkedList.Remove(8));
+            Console.WriteLine(doublyLinkedList.Remove(0));
+
+            doublyLinkedList.Print();
+
+            Console.WriteLine(doublyLinkedList.Size());
+
+            doublyLinkedList.Add(44);
+            doublyLinkedList.Add(12);
+            doublyLinkedList.Add(52);
+            doublyLinkedList.Add(6);
+            doublyLinkedList.Add(91);
+
+            doublyLinkedList.Print();
+
+            Console.WriteLine(doublyLinkedList.Size());
+
+            var (node44, index) = doublyLinkedList.Find(44);
+
+            var newNode43 = new DoublyLinkedNode<int>(43);
+
+            doublyLinkedList.AddAfter(newNode43, node44);
+
+            doublyLinkedList.Print();
+
+            doublyLinkedList.AddAfter(42, newNode43);
+
+            doublyLinkedList.Print();
+
+            doublyLinkedList.AddFirst(45);
+
+            doublyLinkedList.Print();
+
+            doublyLinkedList.AddFirst(46);
+
+            doublyLinkedList.Print();
+
+            doublyLinkedList.AddLast(92);
+
+            doublyLinkedList.Print();
+
+            doublyLinkedList.AddLast(93);
+
+            doublyLinkedList.Print();
+            Console.WriteLine(doublyLinkedList.Size());
+
+            var intArray = new int[50];
+            doublyLinkedList.CopyTo(intArray, 10);
+
+            Console.WriteLine("New array");
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i] + ",");
+            }
+
         }
 
         public static void OperateOnSinlgyLinkedList()
@@ -19,7 +116,7 @@
             sinlgyLinkedList.Add(23);
             sinlgyLinkedList.Add(8);
             sinlgyLinkedList.Add(22);
-            
+
             Console.WriteLine(sinlgyLinkedList.GetSize());
 
             sinlgyLinkedList.Print();
@@ -39,7 +136,7 @@
             sinlgyLinkedList.Remove(44);
 
             sinlgyLinkedList.Print();
-                        
+
             sinlgyLinkedList.Remove(22);
 
             Console.WriteLine(sinlgyLinkedList.FindLast());
@@ -60,12 +157,12 @@
             sinlgyLinkedList.Print();
 
             sinlgyLinkedList.RemoveLast();
-            
+
             sinlgyLinkedList.Print();
-            
+
             Console.WriteLine(sinlgyLinkedList.GetSize());
             Console.WriteLine(sinlgyLinkedList.FindLast());
-                        
+
             Console.WriteLine(sinlgyLinkedList.Contains(3));
             Console.WriteLine(sinlgyLinkedList.Contains(4));
             Console.WriteLine(sinlgyLinkedList.Contains(1));
