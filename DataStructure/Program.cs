ff8946ddd8a49d2bc0467bcfc01119a42bbcbbe7
Leonardo Jorge <leogojorge@outlook.com>
2025-04-02 10:22:15 -0300

    :sparkles: Adds Data Structure csproj with stack array implementation

---
+++ DataStructure/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStack = new StackArray<string>(10);
+
+            myStack.Push("2");
+            myStack.Push("5");
+            myStack.Push("7");
+            myStack.Push("12");
+            myStack.Push("6");
+
+            myStack.Print();
+
+            Console.WriteLine(myStack.Search("6"));
+
+            myStack.Pop();
+
+            Console.WriteLine(myStack.Search("6"));
+
+            myStack.Print();
+
+            myStack.Clear();
+
+            myStack.Print();
+        }
+    }
+}
---
+++ DataStructure/StackArray.cs
@@ -0,0 +1,72 @@
+﻿namespace DataStructure
+{
+    public class StackArray<T>
+    {
+        public T[]? Array { get; }
+
+        public int Top = -1;
+
+        public StackArray(int size)
+        {
+            this.Array = new T[size];
+        }
+
+        public int Search(T value)
+        {
+            for (int i = 0; i <= Top; i++)
+            {
+                if (this.Array[i].Equals(value))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public void Push(T value)
+        {
+            this.Array[Top + 1] = value;
+            Top++;
+        }
+
+        public T Pop()
+        {
+            var result = this.Array[Top];
+            Top--;
+
+            return result;
+        }
+
+        public T Peek()
+        {
+            return this.Array[Top];
+        }
+
+        public int Size()
+        {
+            return Top - 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return Top == -1;
+        }
+
+        public void Clear()
+        {
+            Top = -1;
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+
+            for (int i = 0; i <= Top; i++)
+            {
+                Console.Write(this.Array[i] + ",");
+            }
+
+            Console.WriteLine("]");
+
+        }
+    }
+}
